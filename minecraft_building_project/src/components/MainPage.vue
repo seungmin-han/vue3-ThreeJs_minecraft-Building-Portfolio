2cfbf8ba35535580ebc5d7fc7ee98f884b58583d
seungmin han <seungmin_han@kakao.com>
2023-02-03 11:49:51 +0900

    Feat: vue2 threejs 예제 코드 vue3로 마이그레이션 테스트

---
+++ minecraft_building_project/src/components/MainPage.vue
@@ -0,0 +1,67 @@
+<template>
+    <div ref="canvas"></div>
+</template>
+
+<script setup>
+    import { ref, onMounted } from 'vue';
+    import * as THREE from 'three';
+    import TrackballControls from 'three-trackballcontrols';
+     const scene = new THREE.Scene();
+    // const composer = new THREE.EffectComposer(new WebGLRenderer())
+    // const effectPass = new THREE.EffectPass(camera, new BloomEffect())
+    const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    const light = new THREE.DirectionalLight('hsl(0, 100%, 100%)');
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshStandardMaterial({
+        side: THREE.FrontSide,
+        color: 'hsl(0, 100%, 50%)',
+        wireframe: false
+    });
+    const cube = new THREE.Mesh(geometry, material);
+    const axes = new THREE.AxesHelper(5);
+    const speed = 0.01;
+
+    let controls = [];
+    const canvas = ref(null);
+
+    scene.add(camera);
+    scene.add(light);
+    scene.add(cube);
+    scene.add(axes);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    light.position.set(0, 0, 10);
+    camera.position.z = 5;
+    scene.background = new THREE.Color('hsl(0, 100%, 100%)');
+    console.log(TrackballControls);
+
+    controls = new TrackballControls(camera, renderer.domElement);
+    controls.rotateSpeed = 1.0;
+    controls.zoomSpeed = 5;
+    controls.panSpeed = 0.8;
+    controls.noZoom = false;
+    controls.noPan = false;
+    controls.staticMoving = true;
+    controls.dynamicDampingFactor = 0.3;
+    
+    onMounted(() => {
+        canvas.value.appendChild(renderer.domElement);
+        animate();
+    })
+    const animate = () => {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+        cube.rotation.y += speed;
+        controls.update();
+    }
+        
+</script>
+
+<style>
+
+</style>
