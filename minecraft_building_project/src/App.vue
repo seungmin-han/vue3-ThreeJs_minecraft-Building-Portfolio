1b2fade463afe844493b78de461347c4eb2438f1
seungmin han <seungmin_han@kakao.com>
2023-02-03 17:56:08 +0900

    Revert "Delete minecraft_building_project directory"
    This reverts commit c60258e73bcd53a706c04b37b02d8b6295a33c18.

---
+++ minecraft_building_project/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <!-- <MainPage></MainPage> -->
+    <ObjLoadTest></ObjLoadTest>
+</template>
+
+<script setup>
+import MainPage from './components/MainPage.vue';
+import ObjLoadTest from './components/ObjLoadTest.vue';
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
