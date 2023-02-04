cab0e6a7655b3a74d509e3b1d0aab495a2479c79
seungmin han <seungmin_han@kakao.com>
2023-02-04 17:08:19 +0900

    Update: 코드 리펙토링(홤수화)

--- minecraft_building_project/src/components/ObjLoadTest.vue
+++ minecraft_building_project/src/components/ObjLoadTest.vue
@@ -7,84 +7,78 @@
     import { OBJLoader } from '../modules/OBJLoader';
     import { MTLLoader } from '../modules/MTLLoader';
     import * as THREE from 'three';
-    // import { OBJLoader } from 'three-obj-mtl-loader';
     import TrackballControls from 'three-trackballcontrols';
 
+    const props = defineProps({
+      fileName: String
+    })
 
-    const ASSET_FOLDER_NAME = 'nakagin';
-    const FILE_NAME = 'nakagin';
+    const ASSET_FOLDER_NAME = 'exllu';
+    const FILE_NAME = 'exllu';
 
     let globalObj = reactive(null);
 
     const scene = new THREE.Scene();
-    // const composer = new THREE.EffectComposer(new WebGLRenderer())
-    // const effectPass = new THREE.EffectPass(camera, new BloomEffect())
     const camera = new THREE.PerspectiveCamera(
         75,
-        window.innerWidth / window.innerHeight,
+        300 / 500,
         0.1,
         2000
     );
     const renderer = new THREE.WebGLRenderer({ antialias: true });
-    const light = new THREE.DirectionalLight('hsl(44, 100%, 100%)');
+    // const light = new THREE.DirectionalLight('hsl(44, 100%, 100%)'); // 방향을 갖는 조명
+    const light = new THREE.AmbientLight(0xffffff); // 맵 전체 조명
+    // const light = new THREE.HemisphereLight(0xffffff, 0xff0000, 1); // 하늘 -> 땅 까지의 조명 (그라데이션?)
     let controls = reactive([]);
     const canvas = ref(null);
-    const axes = new THREE.AxesHelper(5);
-    scene.add(axes);
+
+    // 축 객체 (테스트 시 활성화)
+
+    // const axes = new THREE.AxesHelper(5);
+    // scene.add(axes);
     scene.add(camera);
     scene.add(light);
 
-    const mtlLoader = new MTLLoader();
-    mtlLoader.setPath(`/assets/${ASSET_FOLDER_NAME}/`);
-    mtlLoader.load(`${FILE_NAME}.mtl`, 
+    const loadOBJ = (materials) => {
+        const objLoader = new OBJLoader();
+        objLoader.setMaterials(materials);
+        objLoader.setPath(`/assets/${ASSET_FOLDER_NAME}/`);
+        objLoader.load(`${FILE_NAME}.obj`, 
+            obj => {
+                let box = new THREE.Box3().setFromObject(obj);
+                const center = box.getCenter(new THREE.Vector3());
+                obj.position.set(-center.x, -center.y, -center.z);
+                let tmp = new THREE.Group();
+                scene.add(tmp);
+                tmp.add(obj);
+                // scene.add(obj);
+                globalObj = tmp;
+            }, 
+            xhr => {
+                console.log(xhr.loaded / xhr.total * 100 + '% loaded')
+            }, 
+            err=>{
+                console.log('obj load error:', err);
+        });
+    }
+
+    const loadMTL = () => {
+        const mtlLoader = new MTLLoader();
+        mtlLoader.setPath(`/assets/${ASSET_FOLDER_NAME}/`);
+        mtlLoader.load(`${FILE_NAME}.mtl`, 
             materials => {
                 materials.preload();
-                const objLoader = new OBJLoader();
-                objLoader.setMaterials(materials);
-                objLoader.setPath(`/assets/${ASSET_FOLDER_NAME}/`);
-                objLoader.load(`${FILE_NAME}.obj`, 
-                    obj => {
-                        // const box3 = new THREE.Box3().setFromObject(obj);
-                        // const size = box3.getSize(new THREE.Vector3());
-                        // console.log(obj);
-                        // const center = box3.getCenter(new THREE.Vector3());
-                        // obj.position.set(-center.x, -center.y, -center.z);
-                        // const MAX_SIZE = Math.max(size.x, size.y, size.z);
-                        // obj.scale.x = 1 / MAX_SIZE;
-                        // obj.scale.y = 1 / MAX_SIZE;
-                        // obj.scale.z = 1 / MAX_SIZE;
+                loadOBJ(materials);
+            }, 
+            xhr => {
+                console.log(xhr.loaded / xhr.total * 100 + '% loaded')
+            }, 
+            err=>{
+                console.log('mtl load error:', err);
+            });
+    }
 
-                        let box = new THREE.Box3().setFromObject(obj);
-                        const center = box.getCenter(new THREE.Vector3());
-                        obj.position.set(-center.x, -center.y, -center.z);
-                        let tmp = new THREE.Group();
-                        scene.add(tmp);
-                        tmp.add(obj);
-                        // scene.add(obj);
-                        globalObj = tmp;
-                    }, 
-                    xhr => {
-                        console.log(xhr.loaded / xhr.total * 100 + '% loaded')
-                    }, 
-                    err=>{
-                        console.log('obj load error:', err);
-                    });
-                    }, 
-                    xhr => {
-                        console.log(xhr.loaded / xhr.total * 100 + '% loaded')
-                    }, 
-                    err=>{
-                        console.log('mtl load error:', err);
-                    });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    light.position.set(0, 5, 10);
-    camera.position.y = 1000;
-    camera.position.z = 0;
-    camera.position.x = 10;
-    scene.background = new THREE.Color('hsl(0, 100%, 100%)');
-    
-    onMounted(() => {
-        canvas.value.appendChild(renderer.domElement);
+    const createControls = () => {
         controls = new TrackballControls(camera, renderer.domElement);
         controls.rotateSpeed = 3.0;
         controls.zoomSpeed = 5;
@@ -94,6 +88,20 @@
         controls.noRotate = false;
         controls.staticMoving = true;
         controls.dynamicDampingFactor = 0.3;
+    }
+
+    loadMTL();
+    renderer.setSize(300, 500);
+    // light.position.set(0, 5, 10);
+    camera.position.y = 25;
+    camera.position.z = 100;
+    camera.position.x = 100;
+    camera.lookAt(0,25,0);
+    scene.background = new THREE.Color('hsl(0, 100%, 100%)');
+    
+    onMounted(() => {
+        canvas.value.appendChild(renderer.domElement);
+        createControls();
         animate();
     })
     const animate = () => {
