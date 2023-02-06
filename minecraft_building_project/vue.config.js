0c80ffdad851e70a604fd0900833d79636bcc28b
seungmin han <seungmin_han@kakao.com>
2023-02-06 15:24:24 +0900

    Add: BuildFile

--- minecraft_building_project/vue.config.js
+++ minecraft_building_project/vue.config.js
@@ -3,5 +3,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === "production" ? "/vue3-ThreeJs_minecraft-Building-Portfolio/" : "/",
+  outputDir: '../docs',
 })
