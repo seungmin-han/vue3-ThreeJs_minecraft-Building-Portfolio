2e8c1842faa2b63939f952d99a39760ad2b84bb5
seungmin han <seungmin_han@kakao.com>
2023-02-06 16:34:05 +0900

    Update: public Path

--- minecraft_building_project/vue.config.js
+++ minecraft_building_project/vue.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  outputDir: './docs',
-  publicPath: 'vue3-ThreeJs_minecraft-Building-Portfolio',
+  outputDir: '/docs',
+  publicPath: '/vue3-ThreeJs_minecraft-Building-Portfolio/',
 })
