97ff9088ce9cf01fdca744dbd6dd6c242f23e5e7
seungmin han <seungmin_han@kakao.com>
2023-02-07 14:47:56 +0900

    Feat: zoom In/Out 이벤트 구현 및 라이트 추가

--- minecraft_building_project/src/components/ObjLoadTest.vue
+++ minecraft_building_project/src/components/ObjLoadTest.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="item">
         <h2>{{props.fileName}}</h2>
-        <div class="wrapper" ref="canvas">
+        <div class="wrapper" ref="canvas" :class="isZoom ? 'zoom' : 'normal'">
             <div v-if="isLoadEnd" class="btn-group">  
                 <button @click="setRotate(0.01)">회전</button>
                 <button @click="setRotate(0)">멈춤</button>
+                <button @click="zoom">{{ isZoom ? '축소' : '확대' }}</button>
             </div>
             <div v-else class="loading">
                 Loading...
@@ -27,8 +28,30 @@
         fileName: String
     });
 
+    const isZoom = ref(false);
     const rotate = ref(0);
     const setRotate = value => rotate.value = value;
+
+    const zoomIn = () => {
+        isZoom.value = false;
+        camera.aspect = 300 / 500;
+        renderer.setSize(300, 500);
+    }
+
+    const zoomOut = () => {
+        isZoom.value = true;
+        camera.aspect = window.innerWidth / window.innerHeight;
+        renderer.setSize(window.innerWidth -100, window.innerHeight- 100);
+    }
+
+    const zoom = () => {
+        if(isZoom.value) {
+            zoomIn();
+        } else {            
+            zoomOut();
+        }
+        camera.updateProjectionMatrix();
+    }
 
     let globalObj = reactive({});
 
@@ -41,7 +64,11 @@
     );
     const renderer = new THREE.WebGLRenderer({antialias: true, alpha:true});
     // const renderer = new THREE.WebGLRenderer({alpha:true});
-    const light = new THREE.AmbientLight(0xffffff); // 맵 전체 조명
+    const light1 = new THREE.SpotLight(0xffffff, 3); // 스포트 라이트 조명
+    const light2 = new THREE.SpotLight(0xffffff, 3); // 스포트 라이트 조명
+    light1.lookAt(0,0,0);
+    light2.lookAt(0,0,0);
+    // const light = new THREE.AmbientLight(0xffffff, 3); // 맵 전체 조명
     /**  const light = new THREE.DirectionalLight('hsl(44, 100%, 100%)'); // 방향을 갖는 조명
      * const light = new THREE.SpotLight(0xffffff); // 스포트 라이트 조명
      * const light = new THREE.HemisphereLight(0xffffff, 0xffffff, 1); // 하늘 -> 땅 까지의 조명 (그라데이션?)
@@ -55,7 +82,8 @@
     // const axes = new THREE.AxesHelper(5);
     // scene.add(axes);
     scene.add(camera);
-    scene.add(light);
+    scene.add(light1);
+    scene.add(light2);
 
     const loadOBJ = async (materials) => {
         const objLoader = new OBJLoader();
@@ -64,7 +92,7 @@
         objLoader.load(`${props.fileName}.obj`, 
             obj => {
                 if(props.fileName == 'market') {
-                    obj.rotation.y = 180;
+                    obj.rotation.y = 110;
                 }
                 let box = new THREE.Box3().setFromObject(obj);
                 const size = box.getSize(new THREE.Vector3());
@@ -74,11 +102,14 @@
                 camera.position.y = size.y / 2;
                 camera.position.z = MAX_SIZE;
                 camera.position.x = MAX_SIZE;
+                light1.position.set(MAX_SIZE,MAX_SIZE,MAX_SIZE);
+                light2.position.set(-MAX_SIZE,MAX_SIZE,-MAX_SIZE);
                 let tmp = new THREE.Group();
                 scene.add(tmp);
                 tmp.add(obj);
                 // scene.add(obj);
                 globalObj = tmp;
+                globalObj.receiveShadow = true;
                 isLoadEnd.value = true;
                 controls.noZoom = false;
                 controls.noPan = false;
@@ -134,11 +165,12 @@
     loadMTL();
     renderer.setSize(300, 500);
     camera.lookAt(0,25,0);
-    scene.background = new THREE.Color('hsl(0, 100%, 100%)');
+    scene.background = new THREE.Color('hsl(0, 100%, 100%)'); // 색, 채도, 명도 (hue, saturation, lightness)
     
     onMounted(() => {
         
     })
+
     const animate = () => {
         requestAnimationFrame(animate);
         renderer.render(scene, camera);
@@ -154,16 +186,28 @@
 
 <style scoped>
     .item {
-        padding: 20px;
+        padding: 20px 20px 40px 20px;
         background-color: #333;
         margin: 10px;
     }
     .wrapper {
         position: relative;
-        width: 300px;
-        height: 500px;
         background-color: #fff;
     }
+
+    .normal {
+        width: 300px;
+        height: 500px;
+    }
+    .zoom {
+        position: fixed;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%,-50%);
+        border: #333 5px solid;
+        z-index: 3000;
+    }
+
     .btn-group {
         position: absolute;
         top: 0;
